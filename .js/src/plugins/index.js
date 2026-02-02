7c72cec7c118ca08348ff1d402edb349713a4840
hydro63 <peter.hudak.63nn@gmail.com>
2026-02-02 23:35:07 +0100

    chore: Publish Plugins

---
+++ .js/src/plugins/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var WanderingInn_1 = __importDefault(require("@plugins/english/WanderingInn"));
+var PLUGINS = [WanderingInn_1.default];
+exports.default = PLUGINS;
